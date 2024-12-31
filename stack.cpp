3c2da49f179f88b86fed5616dd95e7307f6f2256
AlvaroCaffaro <105836282+AlvaroCaffaro@users.noreply.github.com>
2024-12-31 00:52:31 -0300

    Add files via upload
    adding the stack data structure. In this implementation, the list class is used. The advantage of this structure is that all its operations are O(1), which makes it very efficient.

---
+++ stack.cpp
@@ -0,0 +1,39 @@
+#include "stack.h"
+
+template<typename T>
+Stack<T>::Stack(){
+	this->sz = 0;
+}
+
+template<typename T>
+bool Stack<T>::empty(){
+	return (this->size() == 0);
+}
+	 
+template<typename T>
+int Stack<T>::size(){
+	return this->sz;
+}
+
+template<typename T>
+T& Stack<T>::top(){
+	return this->back();
+}
+template<typename T>
+void Stack<T>::push(T value){
+	this->push_back(value);
+	this->sz += 1;
+}
+
+template<typename T>
+void Stack<T>::pop(){
+	this->pop_back();
+	this->sz -=1;
+}
+
+template<typename T>
+void Stack<T>::clear(){
+	this->erase(this->begin(),this->end());
+	this->sz = 0;
+	
+}
---
+++ stack.h
@@ -0,0 +1,16 @@
+#include "double_linked_list.cpp"
+
+template<typename T>
+class Stack: private List<T>{
+private:
+	int sz;
+public:
+	Stack();
+	bool empty(); 
+	int size();
+	T& top(); 
+	void push(T value);
+	void pop(); 
+	void clear();
+	
+};
