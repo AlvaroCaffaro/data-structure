22bc0ea2fa4845d4848a30a830eae05a75865ed7
AlvaroCaffaro <105836282+AlvaroCaffaro@users.noreply.github.com>
2024-12-31 03:41:53 -0300

    Add files via upload
    adding the queue data structure. This structure uses the first in first out (FIFO) principle

---
+++ queue.cpp
@@ -0,0 +1,41 @@
+
+#include "queue.h"
+
+template<typename T>
+Queue<T>::Queue(){
+	this->sz = 0;
+}
+
+template<typename T>
+bool Queue<T>::empty(){
+	return (this->sz == 0);
+}
+
+template<typename T>
+int Queue<T>::size(){
+	return this->sz;
+}
+
+template<typename T>
+T& Queue<T>::front(){
+	return *(this->begin()); // list´s method
+}
+template<typename T>
+void Queue<T>::push(T value){
+	this->push_back(value);
+	this->sz += 1;
+}
+
+template<typename T>
+void Queue<T>::pop(){
+	this->erase(this->begin()); // list's method
+	this->sz -=1;
+}
+
+template<typename T>
+void Queue<T>::clear(){
+	this->erase(this->begin(),this->end());
+	this->sz = 0;
+	
+}
+
---
+++ queue.h
@@ -0,0 +1,15 @@
+#include "double_linked_list.cpp"
+template<typename T>
+class Queue: private List<T>{
+private:
+	int sz;
+public:
+	Queue();
+	bool empty();
+	int size();
+	T& front();
+	void pop();
+	void push(T value);
+	void clear();
+	
+};
